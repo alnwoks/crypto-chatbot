145287fc3e71b833807208e736555cfdd53e8255
Al Nwokedike <chalonnwokedike@gmail.com>
2023-03-03 16:45:51 +0100

    update code

--- handler/whatsapp_handler.py
+++ handler/whatsapp_handler.py
@@ -1,11 +1,12 @@
 import os
 import logging
-from twilio.rest import Client
+# from wa_automate_socket_client.client import WhatsappClient
+from wa_automate_socket_client.client import WhatsappClient
 from get_crypto_rates import get_crypto_rates
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Define the default
+# Define the default message template
 MESSAGE_TEMPLATE = "Current buy rate for {currency} on {exchange}: {buy_rate}\nCurrent sell rate for {currency} on {exchange}: {sell_rate}\nResponse time: {response_time:.3f} seconds"
 
 # Define the supported cryptocurrencies and their symbols
@@ -20,30 +21,28 @@ CRYPTO_SYMBOLS = {
 EXCHANGES = [
     'Binance',
     'Coinbase',
-    'Kraken',
-    'Huobi',
-    'Bitfinex',
-    'Bitstamp'
+    # 'Kraken',
+    # 'Huobi',
+    # 'Bitfinex',
+    # 'Bitstamp'
 ]
 
-# Initialize the Twilio client using the account SID and auth token stored in the environment variables
-TWILIO_ACCOUNT_SID = os.environ.get('YOUR_TWILIO_ACCOUNT_SID', '')
-TWILIO_AUTH_TOKEN = os.environ.get('YOUR_TWILIO_AUTH_TOKEN', '')
-TWILIO_PHONE_NUMBER = os.environ.get('YOUR_TWILIO_PHONE_NUMBER', '')
-CLIENT = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+# Initialize the WhatsApp client using the session file stored in the environment variable
+WA_SESSION_FILE = os.environ.get('WA_SESSION_FILE', '')
 
-def send_message_whatsapp(message):
-    """Sends a WhatsApp message containing the specified message text."""
-    message = CLIENT.messages.create(
-        from_='whatsapp:' + TWILIO_PHONE_NUMBER,
-        to='whatsapp:' + os.environ.get('YOUR_WHATSAPP_NUMBER', ''),
-        body=message
-    )
-    logging.debug(f"WhatsApp message SID: {message.sid}")
+client = WhatsappClient(
+    client_id=os.environ.get('WA_CLIENT_ID', ''),
+    client_secret=os.environ.get('WA_CLIENT_SECRET', ''),
+    session_path=WA_SESSION_FILE,
+    logger=logging.getLogger(__name__)
+)
 
-def main(event, context):
-    """Main function that sends the latest crypto rates to the user via WhatsApp."""
-    # Get the buy and sell rates for each supported cryptocurrency and exchange
+def send_message(chat_id, message):
+    """Sends a message to a WhatsApp chat."""
+    client.send_message(chat_id, message)
+
+def get_all_rates():
+    """Gets the current buy and sell rates for all supported cryptocurrencies on all supported exchanges."""
     crypto_rates = []
     for crypto in CRYPTO_SYMBOLS.values():
         for exchange in EXCHANGES:
@@ -60,11 +59,33 @@ def main(event, context):
             except Exception as e:
                 error_message = str(e)
                 logging.error(f"Error getting rates from {exchange}: {error_message}")
+    return "\n\n".join(crypto_rates)
 
-    # Send the latest crypto rates to the user via WhatsApp
-    if len(crypto_rates) > 0:
-        message = "\n\n".join(crypto_rates)
-        send_message_whatsapp(message)
+def on_message_received(message):
+    """Handler function for incoming WhatsApp messages."""
+    chat_id = message.chat.id
+    text = message.text
+
+    if text == '/start':
+        response = "Hello! Use the /getallrates command to get the current buy and sell rates for all supported cryptocurrencies on all supported exchanges."
+        send_message(chat_id, response)
+    elif text == '/getallrates':
+        response = get_all_rates()
+        send_message(chat_id, response)
+    else:
+        response = "Invalid command. Please use /start or /getallrates."
+        send_message(chat_id, response)
+
+def main():
+    """Main function for running the WhatsApp bot."""
+    # Connect to the WhatsApp server
+    client.connect()
+
+    # Register the message handler with the WhatsApp client
+    client.register_message_handler(on_message_received)
+
+    # Wait for incoming messages
+    client.wait_for_messages()
 
 if __name__ == '__main__':
     main()
