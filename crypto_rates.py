dd056f04082d784933df4036776ed59390a0c9c4
Al Nwokedike <chalonnwokedike@gmail.com>
2023-03-06 14:25:04 +0100

    update code with libs

--- crypto_rates.py
+++ crypto_rates.py
@@ -1,40 +1,53 @@
 import os
 import logging
+import certifi
 from binance.client import Client as BinanceClient
 from coinbase.wallet.client import Client as CoinbaseClient
-import certifi
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 BINANCE_API_KEY = os.environ.get('BINANCE_API_KEY')
 BINANCE_SECRET_KEY = os.environ.get('BINANCE_SECRET_KEY')
 COINBASE_API_KEY = os.environ.get('COINBASE_API_KEY')
 COINBASE_API_SECRET = os.environ.get('COINBASE_API_SECRET')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
 def get_crypto_rates():
-    # Initialize the Binance client using the API key and secret stored in environment variables
-    client_binance = BinanceClient(api_key=os.environ.get('BINANCE_API_KEY', ''), api_secret=os.environ.get('BINANCE_SECRET_KEY', ''), tld='us', requests_params={'verify': certifi.where()})
+    try:
+        # Initialize the Binance client using the API key and secret stored in environment variables
+        client_binance = BinanceClient(api_key=BINANCE_API_KEY, api_secret=BINANCE_SECRET_KEY, tld='us', requests_params={'verify': certifi.where()})
 
-    # Initialize the Coinbase client using the API key and secret stored in environment variables
-    client_coinbase = CoinbaseClient(api_key=os.environ.get('COINBASE_API_KEY', ''), api_secret=os.environ.get('COINBASE_API_SECRET', ''))
+        # Initialize the Coinbase client using the API key and secret stored in environment variables
+        client_coinbase = CoinbaseClient(api_key=COINBASE_API_KEY, api_secret=COINBASE_API_SECRET)
 
-    crypto_pairs = ['BTC-USD', 'ETH-USD', 'DOGE-USD', 'XRP-USD']
-    crypto_rates = []
+        binance_symbols = {'BTCUSDT', 'ETHUSDT', 'DOGEUSDT', 'XRPUSDT'}
+        coinbase_symbols = {'BTC-USD', 'ETH-USD', 'DOGE-USD', 'XRP-USD'}
+        crypto_rates = []
 
-    for pair in crypto_pairs:
-        try:
-            ticker_binance = client_binance.get_symbol_ticker(symbol=pair)
-            ticker_coinbase = client_coinbase.get_exchange_rates(currency=pair)
+        for symbol in binance_symbols:
+            ticker_binance = client_binance.get_symbol_ticker(symbol=symbol)
+
+            if 'BTC' in symbol:
+                symbol_coinbase = 'BTC-USD'
+            elif 'ETH' in symbol:
+                symbol_coinbase = 'ETH-USD'
+            elif 'DOGE' in symbol:
+                symbol_coinbase = 'DOGE-USD'
+            elif 'XRP' in symbol:
+                symbol_coinbase = 'XRP-USD'
+
+            ticker_coinbase = client_coinbase.get_exchange_rates(currency=symbol_coinbase)
 
             rate = {
-                'symbol': pair,
+                'symbol': symbol,
                 'binance': float(ticker_binance['price']),
                 'coinbase': float(ticker_coinbase['rates']['USD']),
             }
 
             crypto_rates.append(rate)
-        except Exception as e:
-            logger.error(f"Failed to get rates for {pair}: {e}")
 
-    return crypto_rates
+        return crypto_rates
+    except Exception as e:
+        logger.error(f'Error getting crypto rates: {e}')
+        return []
