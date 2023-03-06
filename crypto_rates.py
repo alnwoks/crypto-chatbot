187323b89ffde330d03b65c121ae00b6f8ff4f47
Al Nwokedike <chalonnwokedike@gmail.com>
2023-03-06 22:16:12 +0100

    update release with function

--- crypto_rates.py
+++ crypto_rates.py
@@ -21,30 +21,29 @@ def get_crypto_rates():
         # Initialize the Coinbase client using the API key and secret stored in environment variables
         client_coinbase = CoinbaseClient(api_key=COINBASE_API_KEY, api_secret=COINBASE_API_SECRET)
 
-        crypto_pairs = {
-            'BTCUSDT': {'binance': 'BTCUSD', 'coinbase': 'BTC-USD'},
-            'ETHUSDT': {'binance': 'ETHUSD', 'coinbase': 'ETH-USD'},
-            'DOGEUSDT': {'binance': 'DOGEUSD', 'coinbase': 'DOGE-USD'},
-            'XRPUSDT': {'binance': 'XRPUSD', 'coinbase': 'XRP-USD'}
-        }
+        crypto_pairs_binance = {'BTCUSDT': 'BTCUSD', 'ETHUSDT': 'ETHUSD', 'DOGEUSDT': 'DOGEUSD', 'XRPUSDT': 'XRPUSD'}
+        crypto_pairs_coinbase = {'BTCUSD': 'BTC-USD', 'ETHUSD': 'ETH-USD', 'DOGEUSD': 'DOGE-USD', 'XRPUSD': 'XRP-USD'}
         crypto_rates = []
 
-        for symbol, pairs in crypto_pairs.items():
-            ticker_binance = client_binance.get_symbol_ticker(symbol=symbol)
-            ticker_coinbase = client_coinbase.get_exchange_rates(currency=pairs['coinbase'])
-            bid_price_binance = client_binance.get_order_book(symbol=symbol)['bids'][0][0]
-            ask_price_binance = client_binance.get_order_book(symbol=symbol)['asks'][0][0]
+        for binance_pair, coinbase_pair in zip(crypto_pairs_binance.keys(), crypto_pairs_coinbase.keys()):
+            logger.info(f'Fetching rate for binance pair: {binance_pair}, coinbase pair: {coinbase_pair}')
+            ticker_binance = client_binance.get_symbol_ticker(symbol=binance_pair)
+            ticker_coinbase = client_coinbase.get_exchange_rates(currency=coinbase_pair)
 
             rate = {
-                'symbol': symbol,
-                'binance_buy': float(ask_price_binance),
-                'binance_sell': float(bid_price_binance),
-                'coinbase_buy': float(ticker_coinbase['rates']['USD']),
-                'coinbase_sell': float(ticker_coinbase['rates']['USD'])
+                'symbol': binance_pair,
+                'binance': {
+                    'buy': client_binance.get_ticker(symbol=binance_pair)['bidPrice'],
+                    'sell': client_binance.get_ticker(symbol=binance_pair)['askPrice']
+                },
+                'coinbase': {
+                    'buy': ticker_coinbase['buy'],
+                    'sell': ticker_coinbase['sell']
+                }
             }
 
             crypto_rates.append(rate)
-            logger.info(f'crypto rates: {crypto_rates}')
+        logger.info(f'crypto rates: {crypto_rates}')
         return crypto_rates
     except Exception as e:
         logger.error(f'Error getting crypto rates: {e}')
